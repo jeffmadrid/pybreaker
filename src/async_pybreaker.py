b0aec5b68d5eacda3168ee06f9ca10ae87262c6a
Jefferson Madrid <jefferson.imadrid@gmail.com>
2020-08-29 20:37:25 +1000

    WIP: Extended CircuitBreaker class for async

--- src/async_pybreaker.py
+++ src/async_pybreaker.py
@@ -1,6 +1,25 @@
 #-*- coding:utf-8 -*-
+from functools import wraps
+
 import pybreaker
 
 
 class AsyncCircuitBreaker(pybreaker.CircuitBreaker):
-    pass
+
+    async def call_async(self, func, *args, **kwargs):
+        with self._lock:
+            return await self.state.call_async(func, *args, **kwargs)
+
+    def __call__(self, *call_args, **call_kwargs):
+
+        def _outer_wrapper(func):
+            @wraps(func)
+            async def _inner_wrapper(*args, **kwargs):
+                return await self.call_async(func, *args, **kwargs)
+            return _inner_wrapper
+
+        if call_args:
+            return _outer_wrapper(*call_args)
+        return _outer_wrapper
+
+
